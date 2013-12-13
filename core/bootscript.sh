5ba8b250f2c54f6e806c93e9de6701efcddb8e0b
Dave Green <david.green@tookitaway.co.uk>
2013-12-13 23:43:27 +0000

    Add comments for module loading on startup.

--- core/bootscript.sh
+++ core/bootscript.sh
@@ -4,6 +4,10 @@
 modprobe w1-therm
 modprobe w1-gpio
 
+#if you want to make these modules load on startup, you can run the following (as sudo/root)
+#echo "w1-gpio" >> /etc/modules
+#echo "w1-therm" >> /etc/modules
+
 #set the gpio pins up for correct communication
 #gpio -g mode 27 out
 gpio export 27 out
