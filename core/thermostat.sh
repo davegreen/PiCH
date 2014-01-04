a49e8d5d75675f238b8ebb8c9b57685a83dcdd18
jameswillingham <thecommiser@hotmail.com>
2014-01-04 20:46:58 +0000

    Thermostat script now looks for argument 2

--- core/thermostat.sh
+++ core/thermostat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gpio export 27 out 
 gpio -g mode 27 out 
+if [ -z "$2" ]; then /usr/bin/python ./thermostat.py $1 > output.log; else /usr/bin/python ./thermostat.py $1; fi
 
-/usr/bin/python ./thermostat.py $1 > output.log
